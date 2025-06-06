38c5ff81632ed016200017f8e0005c6f52fd6370
ebektur <seraplatform@gmail.com>
2025-06-06 15:30:47 +0200

    noise image

--- src/imageGenerationWithDiffusionModels.jl
+++ src/imageGenerationWithDiffusionModels.jl
@@ -2,10 +2,21 @@ module imageGenerationWithDiffusionModels
 
 using MAT
 using Images
+using Random
 
 function load_digits_data(filepath::String)
     matfile = matread(filepath) 
     return matfile  
+end
+
+function add_noise_to_image(img, t, β; rng = Random.GLOBAL_RNG)
+    if t==0
+        return img
+    end
+    
+    α = 1 - β[t]                                     # retain-signal factor matlab: alpha = 1 - beta(t)
+    ε = randn(rng, eltype(img), size(img))           # Gaussian noise matlab: epsilon = randn(rng, size(img), 'like', img)
+    return sqrt(α) .* img .+ sqrt(β[t]) .* ε         # noise the image
 end
 
 end
--- src/test.jl
+++ src/test.jl
@@ -5,6 +5,8 @@ using ImageView
 
 #filepath = "/Users/maria/Desktop/image_generation.jl/src/SyntheticImages500.mat"
 const FILE_PATH = joinpath(@__DIR__, "dataset", "SyntheticImages500.mat")
+beta =  LinRange(1e-4, 0.02, 500)  
+ts = 0:50:500                         
 
 data = imageGenerationWithDiffusionModels.load_digits_data(FILE_PATH)  # Explicitly reference the module -
 
@@ -20,8 +22,16 @@ if haskey(data, "syntheticImages")
         println("Displaying image #$i ...")
         w = imshow(img)
         sleep(1.0)              # wait 1 second so you can see it
-        ImageView.close(w["gui"]["window"])    
+        ImageView.close(w["gui"]["window"]) 
+        
+        #Add noise to the image
+        #for each number, plot 11 images from t=0 to t=500 next to each other         
+        frames = [imageGenerationWithDiffusionModels.add_noise_to_image(images[:, :, 1, i], t, beta) for t in ts]
+        w = imshow(hcat(frames...); name = "digit $i  (t = 0 to 500)")
+        sleep(2.0)
+        ImageView.close(w["gui"]["window"])
     end
 else
     println("Field 'syntheticImages' not found.")
 end
+
