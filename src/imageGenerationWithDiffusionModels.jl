f01437c18a46b142cc7c22188b476df24eeb1597
“MaximHaef” <haefner.maxim@gmail.com>
2025-06-09 22:20:06 +0200

    added doc to add_noise_to_image

--- src/imageGenerationWithDiffusionModels.jl
+++ src/imageGenerationWithDiffusionModels.jl
@@ -18,7 +18,20 @@ function load_digits_data(filepath::String)
     return matread(filepath)  
 end
 
-# TODO docs 
+"""
+    add_noise_to_image(img::Vector{Float64}, noise_step::Int64, alpha_bar::Vector{Float64}; rng = Random.GLOBAL_RNG)
+
+Applies Gaussian noise to an Image.
+
+# Arguments
+- `img::Vector{Float64}` : Input image
+- `noise_step::Int64` : Number of noising steps (in each step, noise is applied to the output of the previous step)
+- `alpha_bar::Vector{Float64}` : Vector of noise parameters. Length must be at least "noise_step". Comupted by taking the Cumulative product of (1-"variance schedule")
+- `rng`: Random number generator. Defaults to: `Random.GLOBAL_RNG`.
+
+# Returns
+A noised version of `img`.
+"""
 function add_noise_to_image(img, noise_step, alpha_bar; rng = Random.GLOBAL_RNG)
     if noise_step==0
         return img
