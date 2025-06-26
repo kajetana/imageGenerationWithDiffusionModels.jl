026a7f2320c30aa13c73be854202428e08241f13
“MaximHaef” <haefner.maxim@gmail.com>
2025-06-26 12:43:48 +0200

    Added the ConditionalSkipConnection and ConditionalChain blocks

--- src/blocks.jl
+++ src/blocks.jl
@@ -31,5 +31,56 @@ end
 # 2× down-sampling (MaxPool) helper
 Downsample() = MaxPool((2,2))
 
-export TResBlock, Downsample
+# 2x up-sampling helper
+Upsample() = Upsample((2,2))
+
+# conditional skip connection block https://liorsinai.github.io/machine-learning/2022/12/29/denoising-diffusion-2-unet.html
+# skip connection for architectures with more than one Input
+struct ConditionalSkipConnection{T,F} <: AbstractParallel
+    layers::T           #skipped layers
+    connection::F       #operation which rejoins output of the skipped layers with input feature maps 
+end
+
+Flux.@functor ConditionalSkipConnection
+
+function (skip::ConditionalSkipConnection)(x, ys...)
+    skip.connection(skip.layers(x, ys...), x)
+end
+
+#ConditionalChain is basically Flux.Chain but it accepts conditional arguments(like time embeddings)
+abstract type AbstractParallel end
+
+
+#dispatch helper
+_maybe_forward(layer::AbstractParallel, x::AbstractArray, ys::AbstractArray...) = 
+    layer(x, ys...)
+_maybe_forward(layer::Parallel, x::AbstractArray, ys::AbstractArray...) = 
+    layer(x, ys...)
+_maybe_forward(layer, x::AbstractArray, ys::AbstractArray...) = 
+    layer(x)
+
+struct ConditionalChain{T<:Union{Tuple,NamedTuple}} <: AbstractParallel
+    layers::T
+end
+Flux.@functor ConditionalChain 
+
+ConditionalChain(xs...) = ConditionalChain(xs)          #Positional constructor
+function ConditionalChain(; kw...)                      #Keyword constructor
+  :layers in keys(kw) && throw(ArgumentError("a Chain cannot have a named layer called `layers`"))
+  isempty(kw) && return ConditionalChain(())
+  ConditionalChain(values(kw))
+end
+
+Flux.@forward ConditionalChain.layers Base.getindex, Base.length, Base.first, Base.last,
+Base.iterate, Base.lastindex, Base.keys, Base.firstindex
+
+Base.getindex(c::ConditionalChain, i::AbstractArray) = ConditionalChain(c.layers[i]...)
+
+function (c::ConditionalChain)(x, ys...) 
+    for layer in c.layers
+        x = _maybe_forward(layer, x, ys...)
+    end
+    x
+end
+export TResBlock, Downsample, Upsample, ConditionalChain, ConditionalSkipConnection
 end 
