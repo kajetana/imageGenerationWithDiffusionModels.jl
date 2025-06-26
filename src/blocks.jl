0c9cc36b8542b2a4b72a8a206b12632cf6a1989e
MaximHaef <haefner.maxim@gmail.com>
2025-06-26 21:12:07 +0200

    Maxim upsampling (#11)
    * Added the ConditionalSkipConnection and ConditionalChain blocks
    
    * added _add_unet_level and cat_on_channel_dim functions
    
    * TResBlock now takes channel information as Pair instead of 2 ints
    
    ---------
    
    Co-authored-by: “MaximHaef” <“haefner.maxim@gmail.com”>

--- src/blocks.jl
+++ src/blocks.jl
@@ -11,11 +11,11 @@ struct TResBlock
     emb_proj::Dense     # emb_dim → out_channel
 end
 
-function TResBlock(in_ch::Int, out_ch::Int, emb_dim::Int)
+function TResBlock(channels::Pair{<:Integer,<:Integer}, emb_dim::Int)
     TResBlock(
-        Conv((3,3), in_ch => out_ch; pad = 1),
-        Conv((3,3), out_ch => out_ch; pad = 1),
-        in_ch == out_ch ? identity : Conv((1,1), in_ch => out_ch),
+        Conv((3,3), channels; pad = 1),
+        Conv((3,3), channels; pad = 1),
+        channels[1] == channels[2] ? identity : Conv((1,1), channels),
         Dense(emb_dim, out_ch)
     )
 end
@@ -31,5 +31,56 @@ end
 # 2× down-sampling (MaxPool) helper
 Downsample() = MaxPool((2,2))
 
-export TResBlock, Downsample
+# 2x up-sampling helper
+Upsample() = Upsample((2,2))
+
+# conditional skip connection block https://liorsinai.github.io/machine-learning/2022/12/29/denoising-diffusion-2-unet.html
+# skip connection for architectures with more than one Input
+struct ConditionalSkipConnection{T,F} <: AbstractParallel
+    layers::T           #skipped layers
+    connection::F       #operation which rejoins output of the skipped layers with input feature maps 
+end
+
+Flux.@functor ConditionalSkipConnection
+
+function (skip::ConditionalSkipConnection)(x, ys...)
+    skip.connection(skip.layers(x, ys...), x)
+end
+
+#ConditionalChain is basically Flux.Chain but it accepts conditional arguments(like time embeddings)
+abstract type AbstractParallel end
+
+
+#dispatch helper
+_maybe_forward(layer::AbstractParallel, x::AbstractArray, ys::AbstractArray...) = 
+    layer(x, ys...)
+_maybe_forward(layer::Parallel, x::AbstractArray, ys::AbstractArray...) = 
+    layer(x, ys...)
+_maybe_forward(layer, x::AbstractArray, ys::AbstractArray...) = 
+    layer(x)
+
+struct ConditionalChain{T<:Union{Tuple,NamedTuple}} <: AbstractParallel
+    layers::T
+end
+Flux.@functor ConditionalChain 
+
+ConditionalChain(xs...) = ConditionalChain(xs)          #Positional constructor
+function ConditionalChain(; kw...)                      #Keyword constructor
+  :layers in keys(kw) && throw(ArgumentError("a Chain cannot have a named layer called `layers`"))
+  isempty(kw) && return ConditionalChain(())
+  ConditionalChain(values(kw))
+end
+
+Flux.@forward ConditionalChain.layers Base.getindex, Base.length, Base.first, Base.last,
+Base.iterate, Base.lastindex, Base.keys, Base.firstindex
+
+Base.getindex(c::ConditionalChain, i::AbstractArray) = ConditionalChain(c.layers[i]...)
+
+function (c::ConditionalChain)(x, ys...) 
+    for layer in c.layers
+        x = _maybe_forward(layer, x, ys...)
+    end
+    x
+end
+export TResBlock, Downsample, Upsample, ConditionalChain, ConditionalSkipConnection
 end 
